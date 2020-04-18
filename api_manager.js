7a8f25f103db58dcb03eb44b8f3afe45b42df643
Minifixio <emile@le-gallic.com>
2020-04-19 00:03:07 +0200

    right port

--- api_manager.js
+++ api_manager.js
@@ -42,6 +42,6 @@ app.get('/api/dev/reset', function(req, res) {
     res.send(true)
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on port 3000!')
 });
